27422263320bc75ffdc18ec0b16a84b362a9c425
P Siva Charan <palleshiva2007@gmail.com>
2026-08-18 18:31:40 +0530

    Time: 1 ms (3.54%), Space: 43.3 MB (7.4%) - LeetHub

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        int value = root.val;
+
+        while (!stack.isEmpty()) {
+
+            TreeNode node = stack.pop();
+
+            if (node.val != value) {
+                return false;
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return true;
+    }
+}
