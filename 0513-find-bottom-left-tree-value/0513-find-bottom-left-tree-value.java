8bc9abfcbb30b52d63106da813dde4f37c6dd733
P Siva Charan <palleshiva2007@gmail.com>
2026-08-29 12:39:30 +0530

    Time: 3 ms (64.7%), Space: 47.2 MB (5.29%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        findDepth(root, 0, map);
+        int maxDepth = 0;
+
+        for (int depth : map.keySet()) {
+            maxDepth = Math.max(maxDepth, depth);
+        }
+
+        return map.get(maxDepth);
+    }
+
+    public void findDepth(TreeNode root, int depth, Map<Integer, Integer> map) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (!map.containsKey(depth)) {
+            map.put(depth, root.val);
+        }
+
+        findDepth(root.left, depth + 1, map);
+        findDepth(root.right, depth + 1, map);
+    }
+}
